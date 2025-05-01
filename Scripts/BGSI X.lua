2f25319c53e87db9150211e8e3093920c1ec1c66
나이트로 <82841829+nitrosaber@users.noreply.github.com>
2025-05-02 00:45:42 +0700

    Update BGSI X.lua

--- Scripts/BGSI X.lua
+++ Scripts/BGSI X.lua
@@ -1,43 +1,9 @@
 -- Load NatUI Library
 local NatUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/Uisource.lua"))()
 
--- Services
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Shared = ReplicatedStorage:WaitForChild("Shared", 10)
-local Framework = Shared and Shared:WaitForChild("Framework", 10)
-local Network = Framework and Framework:WaitForChild("Network", 10)
-local Remote = Network and Network:WaitForChild("Remote", 10)
-
-local RemoteEvent = Remote and Remote:FindFirstChild("Event")
-local RemoteFunction = Remote and Remote:FindFirstChild("Function")
-
 --- Create Window
 NatUI:Window({
 	Title = "NiTro Hub",
 	Description = "made by 2o3b.",
 	Icon = "rbxassetid://6035019070"
 })
-
---- Open Toggle UI
-NatUI:Main = OpenUI({
-	Title = "Main",
-	Icon = "rbxasset",
-	BackgroundColor = "fromrgb",
-	BorderColor = "fromrgb"
-})
-
-
---- Open Toggle UI
-NatUI:Farm = OpenUI({
-	Title = "Farm",
-	Icon = "rbxasset",
-	BackgroundColor = "fromrgb",
-	BorderColor = "fromrgb"
-})
-
---- Section
-NatUI:Farm = Section({
-	Title = "Bubble",
-	Icon = "rbxasset"
-})
-
