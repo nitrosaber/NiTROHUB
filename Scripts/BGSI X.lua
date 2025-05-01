80acccb85fe59f22385aa31a36656dce013b641d
나이트로 <82841829+nitrosaber@users.noreply.github.com>
2025-05-02 00:48:56 +0700

    Update BGSI X.lua

--- Scripts/BGSI X.lua
+++ Scripts/BGSI X.lua
@@ -1,8 +1,8 @@
-local NatUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/Uisource.lua"))()
+local NiTroUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/Uisource.lua"))()
 
 --- Create Window
-NatUI:Window Main({
-	Title = "NatUI Library",
+NiTroUI:Window({
+	Title = "NiTroUI Library",
 	Description = "made by 2o3b.",
 	Icon = "rbxasset"
 })
