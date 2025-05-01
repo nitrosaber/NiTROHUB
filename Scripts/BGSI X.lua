040d6589b881844764b904ff6a788193664edf94
나이트로 <82841829+nitrosaber@users.noreply.github.com>
2025-05-02 00:54:16 +0700

    Update BGSI X.lua

--- Scripts/BGSI X.lua
+++ Scripts/BGSI X.lua
@@ -1,8 +1,9 @@
 local NiTroUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/Uisource.lua"))()
 
---- Create Window
-NiTroUI:Window({
-	Title = "NiTroUI Library",
-	Description = "made by 2o3b.",
-	Icon = "rbxasset"
+--- Open Toggle UI
+NatUI:OpenUI({
+	Title = "nathubui",
+	Icon = "rbxasset",
+	BackgroundColor = "fromrgb",
+	BorderColor = "fromrgb"
 })
